75f8e6007fd4a95135b223eae1bc8156bb4ecd13
neonRepublic <194363997+neonRepublic@users.noreply.github.com>
2025-02-05 20:18:53 -0500

    updated js 2525 1818pm

--- javascript.js
+++ javascript.js
@@ -3,15 +3,15 @@ humanScore = 0;
 computerScore = 0;
 
 // Subproblem #1
-function getHumanScore() {
+function getHumanChoice() {
 
-    let humanChoice = prompt("Rock Paper or Scissors?")
+    let humanChoice = prompt( "rock paper or scissors?" )
 
-    if(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
-        alert("You chose " + humanChoice);
+    if( humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors" ) {
+        alert( "You chose " + humanChoice );
         return humanChoice;
     } else {
-        alert("Invalid Option");
+        alert( "Invalid Option" );
     };
 
 };
@@ -19,8 +19,50 @@ function getHumanScore() {
 // Subproblem #2
 function getComputerChoice() {
 
-    const computerChoice = ["Rock", "Paper", "Scissors"];
-    return computerChoice[Math.floor(Math.random() * 3)];
+    const computerChoice = [ "rock", "paper", "scissors" ];
+    return computerChoice[ Math.floor(Math.random() * 3 )];
+
+};
+
+// Subproblem #5
+function playGame( rounds ) {
+
+    //Subproblem #4
+    function playRound() {
+
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+
+        console.log( "You chose: " + humanChoice );
+        console.log( "Computer chose: " + computerChoice );
+
+        if ( humanChoice === computerChoice ) {
+
+            alert( "It's a tie!" );
+
+        } else if (
+
+            ( humanChoice === "rock" && computerChoice === "scissors" ) ||
+            ( humanChoice === "paper" && computerChoice === "rock" ) ||
+            ( humanChoice === "scissors" && computerChoice === "paper" )
+
+        ) {
+
+            alert("You Win!");
+            humanScore++;
+
+        } else {
+
+            alert("You Lose!");
+            computerScore++;
+
+        };
+
+        console.log( "You: " + humanScore + ", Computer: " + computerScore );
+
+        
+
+    }; 
 
 };
 
